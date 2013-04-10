53f20325d8a0c929b58269252d876c577ec3ecbf
darek.uchman@gmail.com <darek.uchman@gmail.com@d89b2898-3b62-8543-56a4-dfb1e94b60ce>
2013-04-10 19:37:22 +0000

    First draft

---
+++ trunk/CodeCheef/AbstractTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeCheef
+{
+    public interface AbstractTask
+    {
+          void PrepareData();
+          void Run();
+    }
+}
