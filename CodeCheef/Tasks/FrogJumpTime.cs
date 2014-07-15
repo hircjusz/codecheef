3b110e53e6c9716dea90df4de01c85d4ba8255c4
darek.uchman@gmail.com <darek.uchman@gmail.com@d89b2898-3b62-8543-56a4-dfb1e94b60ce>
2014-07-15 12:02:12 +0000

    FrogJumpTest

---
+++ CodeCheef/Tasks/FrogJumpTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeCheef.Tasks
+{
+    public class FrogJumpTime
+    {
+
+        public static int Solution(int x, int[] a, int n)
+        {
+            return 0;
+        }
+    }
+}
---
+++ TestProject/FrogJumpTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using CodeCheef.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class FrogJumpTest
+    {
+        /*
+          A[0] = 1
+          A[1] = 3
+          A[2] = 1
+          A[3] = 4
+          A[4] = 2
+          A[5] = 3
+          A[6] = 5
+          A[7] = 4
+         */
+        [TestMethod]
+        public void SimpleTest()
+        {
+
+            int x = 5;
+            int n = 7;
+            int[] a= new int[8];
+            int result = FrogJumpTime.Solution(x,a,n);
+            Assert.AreEqual(6,result);
+        }
+    }
+}
