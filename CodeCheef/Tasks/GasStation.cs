03dbb973bc6ffcf229af8f754d8da287eb067994
darek.uchman@gmail.com <darek.uchman@gmail.com@d89b2898-3b62-8543-56a4-dfb1e94b60ce>
2015-03-01 14:40:28 +0000

    fp9kR2Hz2nD5

---
+++ CodeCheef/Tasks/GasStation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeCheef.Tasks
+{
+    public class GasStation
+    {
+
+        int[] D = null;
+        double[] P = null;
+        double[,] C = null;
+        int T = 3;
+
+        public void Init()
+        {
+            D = new int[3];
+            D[0] = 0;
+            D[1] = 5;
+            D[2] = 10;
+
+            P = new double[3];
+            P[0] = 1;
+            P[1] = 3.0;
+            P[2] = 4.0;
+
+
+
+
+
+
+        }
+
+
+        public void Run()
+        {
+
+        }
+
+        public void Solution()
+        {
+
+
+
+        }
+    }
+}
