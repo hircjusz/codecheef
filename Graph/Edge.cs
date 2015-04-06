566f0920ec8890c288815b9153ee6e06d6366ff9
hircjusz <darek.uchman@gmail.com>
2015-04-06 14:03:21 +0200

    no message

---
+++ Graph/Edge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public class Edge
+    {
+        public int V { get; set; }
+        public int W { get; set; }
+
+        public Edge(int v, int w)
+        {
+
+        }
+
+    }
+}
---
+++ Graph/IGraph.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    interface IGraph
+    {
+
+        int V { get; }
+        int E { get; }
+        bool IsDirected { get; }
+        int Insert();
+
+
+    }
+}
